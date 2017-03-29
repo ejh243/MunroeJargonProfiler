c30deb93e79b4f96589e1f1e3c56ffe283b173aa
Jonathan Cooper <j.p.cooper@ucl.ac.uk>
2017-03-29 07:47:00 +0100

    Start python package infrastructure
    This is a first stab at setup.py and requirements files.
    We need to consider a PEP8-compliant package name.

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(name='JargonMeter',
+      version='0.1',
+      description='Assessing the jargon content of texts and speech',
+      url='https://github.com/ejh243/MunroeJargonProfiler',
+      author='Eilis Hannon, Jonathan Cooper, Mateusz Kuzak, Raniere Silva, Andrew Hufton, Emma Tattershall',
+      author_email='',
+      license='MIT',
+      classifiers=['Development Status :: 3 - Alpha',
+                   'Programming Language :: Python',
+                   'Programming Language :: Python :: 3',
+                   'Operating System :: OS Independent',
+                   'Intended Audience :: Science/Research',
+                   'License :: OSI Approved :: MIT License'],
+      install_requires=['nltk'],
+      python_requires='>=3.3',
+      packages=find_packages(exclude=['*test']),
+      zip_safe=False
+      )
