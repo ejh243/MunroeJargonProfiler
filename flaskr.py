22610515add765ce57decfbb5c5afebcb8907966
Raniere Silva <raniere@rgaiacs.com>
2017-03-29 10:25:24 +0100

    Add text box

--- flaskr.py
+++ flaskr.py
@@ -1,13 +1,22 @@
 from flask import Flask, request, session, g, redirect, url_for, \
      abort, render_template, flash
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired
+
+SECRET_KEY = 'development key'
 
 app = Flask(__name__)
 app.config.from_object(__name__)
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
+class TextForm(FlaskForm):
+    text = StringField('text')
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    form = TextForm()
+    return render_template('index.html', form=form)
 
 if __name__ == '__main__':
     app.run()
