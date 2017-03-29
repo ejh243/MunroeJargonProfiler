3fba243ee30d25ca0096aee2f9ee048b6bcbd961
Raniere Silva <raniere@rgaiacs.com>
2017-03-29 15:04:56 +0100

    Colour words at report

--- flaskr.py
+++ flaskr.py
@@ -36,13 +36,26 @@ def index():
         score = munroe.munroe_score(text)
         return render_template(
             'report.html',
-            text=text,
+            text=colour_text(text, score["tagged_words"]),
             score=score["score"]
         )
     return render_template(
         'index.html',
         form=form
     )
+
+def colour_text(text, word_list):
+    """Add colour to text."""
+    list_of_words = text.split(" ")  # FIXME This will not get a good match
+    coloured_list_of_words = []
+
+    for word in list_of_words:
+        if word in word_list:
+            coloured_list_of_words.append("<span class=\"text-warning\">{}</span>".format(word))
+        else:
+            coloured_list_of_words.append(word)
+
+    return " ".join(coloured_list_of_words)
 
 def run(host, port):
     """
