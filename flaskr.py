4971b8b463bd3494f92ff5b4759745c9cfc4d57f
Raniere Silva <raniere@rgaiacs.com>
2017-03-29 10:10:33 +0100

    Add index

--- flaskr.py
+++ flaskr.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 app.config.from_object(__name__)
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
+@app.route('/')
+def index():
+    return render_template('index.html')
+
 if __name__ == '__main__':
     app.run()
